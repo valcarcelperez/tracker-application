f2a4c5dcb46913669c5f1ddb4bc4758e1bef287c
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-09-07 11:04:43 -0400

    Added Swagger-UI.

--- TrackerApplication/TrackerApplication.WebApi/Startup.cs
+++ TrackerApplication/TrackerApplication.WebApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using TrackerApplication.Contracts;
 using TrackerApplication.Services;
 
@@ -23,6 +24,13 @@ namespace TrackerApplication.WebApi
             services.AddControllers();
 
             services.AddTransient<ITrackerService, TrackerService>();
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TrackerApplication.WebApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +46,12 @@ namespace TrackerApplication.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrackerApplication.WebApi V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
