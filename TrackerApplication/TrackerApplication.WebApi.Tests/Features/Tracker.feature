eb5693f3787acb3667ca415e2064585cb8e4d53e
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-09-07 14:19:06 -0400

    Added TrackerApplication.WebApi.Tests with one test.

---
+++ TrackerApplication/TrackerApplication.WebApi.Tests/Features/Tracker.feature
@@ -0,0 +1,7 @@
+﻿Feature: Tracker
+	Test for Tracker controller in TrackerApplication.WebApi
+
+Scenario: Given that the service just started Retrieve should return zero items
+	When I send a POST request to api/tracker/retrieve
+	Then StatusCode should be 200
+	
---
+++ TrackerApplication/TrackerApplication.WebApi.Tests/TrackerSteps.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Specflow.Steps.WebApi;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TrackerApplication.WebApi.Tests
+{
+    [Binding]
+    [Scope(Feature = "Tracker")]
+    public class TrackerSteps : WebApiSpecs
+    {
+        public const string BaseUrl = "http://localhost:5000";
+
+        private static IWebHost _webHost;
+
+        private static readonly WebApiSpecsConfig _config = new WebApiSpecsConfig { BaseUrl = BaseUrl };
+
+        public TrackerSteps(TestContext testContext) : base(testContext, _config) { }
+
+        [BeforeScenario]
+        public void StartService()
+        {
+            _webHost = WebHost.CreateDefaultBuilder()
+                .UseUrls(BaseUrl)
+                .UseStartup<Startup>()
+                .Build();
+
+            _webHost.Start();
+        }
+
+        [AfterScenario]
+        public void StopService()
+        {
+            _webHost.StopAsync(TimeSpan.FromSeconds(3)).Wait();
+        }
+    }
+}
